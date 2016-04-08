e406ad63a2ad36a74454f5422968969b4579fa88
Maxim Abrosimov <mabrosim@gmail.com>
2016-04-08 17:03:07 +0300

    Added button to clear a text line in the editor

--- app/src/main/java/fi/mabrosim/memowidget/MemoWidgetEditActivity.java
+++ app/src/main/java/fi/mabrosim/memowidget/MemoWidgetEditActivity.java
@@ -3,23 +3,50 @@ package fi.mabrosim.memowidget;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.EditText;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class MemoWidgetEditActivity extends Activity {
-    private final List<EditText> editTexts = new ArrayList<>();
+    private final List<EditText> mEditTexts = new ArrayList<>();
+
+    private static final Map<Integer, Integer> BUTTON_TO_EDITTEXT_MAP;
+
+    private OnClickListener mClearOnClickListener = new OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            ((EditText) findViewById(BUTTON_TO_EDITTEXT_MAP.get(v.getId()))).setText("");
+        }
+    };
+
+    static {
+        Map<Integer, Integer> m = new HashMap<>();
+        m.put(R.id.buttonClear1, R.id.editTextLine1);
+        m.put(R.id.buttonClear2, R.id.editTextLine2);
+        m.put(R.id.buttonClear3, R.id.editTextLine3);
+        m.put(R.id.buttonClear4, R.id.editTextLine4);
+        m.put(R.id.buttonClear5, R.id.editTextLine5);
+        BUTTON_TO_EDITTEXT_MAP = Collections.unmodifiableMap(m);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_memo_edit);
-        editTexts.add((EditText) findViewById(R.id.editTextLine1));
-        editTexts.add((EditText) findViewById(R.id.editTextLine2));
-        editTexts.add((EditText) findViewById(R.id.editTextLine3));
-        editTexts.add((EditText) findViewById(R.id.editTextLine4));
-        editTexts.add((EditText) findViewById(R.id.editTextLine5));
+        mEditTexts.add((EditText) findViewById(R.id.editTextLine1));
+        mEditTexts.add((EditText) findViewById(R.id.editTextLine2));
+        mEditTexts.add((EditText) findViewById(R.id.editTextLine3));
+        mEditTexts.add((EditText) findViewById(R.id.editTextLine4));
+        mEditTexts.add((EditText) findViewById(R.id.editTextLine5));
+
+        for (Integer buttonId : BUTTON_TO_EDITTEXT_MAP.keySet()) {
+            findViewById(buttonId).setOnClickListener(mClearOnClickListener);
+        }
 
         if (!Prefs.isWidgetEnabled(this)) {
             findViewById(R.id.textViewDisabledHint).setVisibility(View.VISIBLE);
@@ -34,7 +61,7 @@ public class MemoWidgetEditActivity extends Activity {
         List<String> texts = Prefs.getTexts(this);
         int count = texts.size();
         for (int i = 0; i < count; i++) {
-            editTexts.get(i).setText(texts.get(i));
+            mEditTexts.get(i).setText(texts.get(i));
         }
     }
 
@@ -43,7 +70,7 @@ public class MemoWidgetEditActivity extends Activity {
         super.onPause();
         final List<String> strings = new ArrayList<>();
 
-        for (EditText et : editTexts) {
+        for (EditText et : mEditTexts) {
             strings.add(et.getText().toString());
         }
 
