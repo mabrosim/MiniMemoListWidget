dc11747d9242507fb83ebe715af0e723ca70dea3
Maxim Abrosimov <mabrosim@gmail.com>
2016-05-15 02:36:57 +0300

    Undo button for a cleared text line
    a new feature to restore
    just cleared text by a button click.

---
+++ app/src/main/java/fi/mabrosim/memowidget/ButtonStateTextWatcher.java
@@ -0,0 +1,59 @@
+package fi.mabrosim.memowidget;
+
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.ImageButton;
+
+public class ButtonStateTextWatcher implements TextWatcher {
+    private final ImageButton mImgBtn;
+
+    enum STATE {
+        INACTIVE,
+        CLEAR,
+        UNDO
+    }
+
+    ButtonStateTextWatcher(ImageButton imgBtn) {
+        mImgBtn = imgBtn;
+    }
+
+    @Override
+    public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+    }
+
+    @Override
+    public void onTextChanged(CharSequence s, int start, int before, int count) {
+    }
+
+    @Override
+    public void afterTextChanged(Editable s) {
+        if (String.valueOf(mImgBtn.getTag(R.id.cachedText)).isEmpty()) {
+            setButtonState(mImgBtn, s.toString().isEmpty() ? STATE.INACTIVE : STATE.CLEAR);
+        } else {
+            setButtonState(mImgBtn, s.toString().isEmpty() ? STATE.UNDO : STATE.CLEAR);
+        }
+    }
+
+    private static void setButtonState(ImageButton imageButton, STATE state) {
+        if (state.equals(imageButton.getTag(R.id.buttonState))) {
+            return;
+        }
+        imageButton.setTag(R.id.buttonState, state);
+        switch (state) {
+            case CLEAR: {
+                imageButton.setImageResource(R.drawable.ic_clear_black_24dp);
+                imageButton.setTag(R.id.cachedText, "");
+                break;
+            }
+            case UNDO: {
+                imageButton.setImageResource(R.drawable.ic_undo_black_24dp);
+                break;
+            }
+            default:
+            case INACTIVE: {
+                imageButton.setImageResource(R.drawable.ic_clear_grey_400_24dp);
+                break;
+            }
+        }
+    }
+}
--- app/src/main/java/fi/mabrosim/memowidget/MemoWidgetEditActivity.java
+++ app/src/main/java/fi/mabrosim/memowidget/MemoWidgetEditActivity.java
@@ -3,55 +3,35 @@ package fi.mabrosim.memowidget;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
-import android.view.View.OnClickListener;
 import android.widget.EditText;
+import android.widget.ImageButton;
 
 import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashMap;
+import java.util.Arrays;
 import java.util.List;
-import java.util.Map;
 
 public class MemoWidgetEditActivity extends Activity {
-    private final List<EditText> mEditTexts = new ArrayList<>();
-
-    private static final Map<Integer, Integer> BUTTON_TO_EDITTEXT_MAP;
-
-    private OnClickListener mClearOnClickListener = new OnClickListener() {
-        @Override
-        public void onClick(View v) {
-            ((EditText) findViewById(BUTTON_TO_EDITTEXT_MAP.get(v.getId()))).setText("");
-        }
-    };
-
-    static {
-        Map<Integer, Integer> m = new HashMap<>();
-        m.put(R.id.buttonClear1, R.id.editTextLine1);
-        m.put(R.id.buttonClear2, R.id.editTextLine2);
-        m.put(R.id.buttonClear3, R.id.editTextLine3);
-        m.put(R.id.buttonClear4, R.id.editTextLine4);
-        m.put(R.id.buttonClear5, R.id.editTextLine5);
-        BUTTON_TO_EDITTEXT_MAP = Collections.unmodifiableMap(m);
-    }
+    private final        List<EditText> mEditTexts  = new ArrayList<>();
+    private static final int[]          editTextIds = {R.id.editTextLine1, R.id.editTextLine2, R.id.editTextLine3, R.id.editTextLine4, R.id.editTextLine5};
+    private static final List<Integer>  buttonIds   = Arrays.asList(R.id.button1, R.id.button2, R.id.button3, R.id.button4, R.id.button5);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_memo_edit);
-        mEditTexts.add((EditText) findViewById(R.id.editTextLine1));
-        mEditTexts.add((EditText) findViewById(R.id.editTextLine2));
-        mEditTexts.add((EditText) findViewById(R.id.editTextLine3));
-        mEditTexts.add((EditText) findViewById(R.id.editTextLine4));
-        mEditTexts.add((EditText) findViewById(R.id.editTextLine5));
 
-        for (Integer buttonId : BUTTON_TO_EDITTEXT_MAP.keySet()) {
-            findViewById(buttonId).setOnClickListener(mClearOnClickListener);
+        for (int i = 0; i < editTextIds.length; i++) {
+            EditText editText = (EditText) findViewById(editTextIds[i]);
+            ImageButton imageButton = (ImageButton) findViewById(buttonIds.get(i));
+
+            mEditTexts.add(editText);
+            editText.addTextChangedListener(new ButtonStateTextWatcher(imageButton));
+            imageButton.setTag(R.id.cachedText, "");
         }
 
         if (!Prefs.isWidgetEnabled(this)) {
             findViewById(R.id.textViewDisabledHint).setVisibility(View.VISIBLE);
         }
-
         setResult(RESULT_OK);
     }
 
@@ -69,12 +49,31 @@ public class MemoWidgetEditActivity extends Activity {
     protected void onPause() {
         super.onPause();
         final List<String> strings = new ArrayList<>();
-
         for (EditText et : mEditTexts) {
             strings.add(et.getText().toString());
         }
-
         Prefs.saveTexts(this, strings);
         MemoWidget.updateMemoWidget(this);
+    }
+
+    public void onButtonClick(View view) {
+        switch ((ButtonStateTextWatcher.STATE) view.getTag(R.id.buttonState)) {
+            case CLEAR: {
+                EditText editText = (EditText) findViewById(editTextIds[buttonIds.indexOf(view.getId())]);
+                view.setTag(R.id.cachedText, editText.getText().toString());
+                editText.setText("");
+                break;
+            }
+            case UNDO: {
+                String text = (String) view.getTag(R.id.cachedText);
+                view.setTag(R.id.cachedText, "");
+                ((EditText) findViewById(editTextIds[buttonIds.indexOf(view.getId())])).setText(text);
+                break;
+            }
+            default:
+            case INACTIVE: {
+                break;
+            }
+        }
     }
 }
